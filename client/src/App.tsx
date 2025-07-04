92aa3dedb5edc70eb9832a84c98dd7e17bb34ccc
Levitag12 <levitaa.g12@gmail.com>
2025-07-04 19:36:30 +0000

    correção app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+// --- LINHA CORRIGIDA ---
+// O erro estava aqui. A importação estava com 'a/' em vez de '@/'.
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { useAuth } from "@/hooks/useAuth";
