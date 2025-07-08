ad9bb85163f80c3e4924f83b7799c28aa0feaa91
Levitag12 <levitaa.g12@gmail.com>
2025-07-08 12:52:57 +0000

    build: adiciona script de build com npm install

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
-echo "Instalando dependências e construindo frontend..."
+#!/usr/bin/env bash
+
+# Instala as dependências do back-end
+npm install
+
+# Instala as dependências do front-end
 cd client
 npm install
+
+# Builda o front-end
 npm run build
+
+# Volta para a raiz
 cd ..
+
+# Agora sim, pode buildar o backend (se necessário)
