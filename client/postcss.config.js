a0f9d0c96c21199f3d640ae00ae61c1e32678b63
Levitag12 <levitaa.g12@gmail.com>
2025-07-08 13:36:30 +0000

    correção

--- client/postcss.config.js
+++
@@ -1,6 +0,0 @@
-import tailwindcss from 'tailwindcss/postcss'; // ✅ novo caminho para Tailwind 4+
-import autoprefixer from 'autoprefixer';
-
-export default {
-  plugins: [tailwindcss, autoprefixer],
-};
