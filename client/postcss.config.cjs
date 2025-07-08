b36d74b42fb64fa1c83c7ebe72577e92b4a0ef43
Levitag12 <levitaa.g12@gmail.com>
2025-07-08 14:20:13 +0000

    corrigido

--- client/postcss.config.cjs
+++ client/postcss.config.cjs
@@ -1,7 +1,7 @@
 // client/postcss.config.cjs
 module.exports = {
   plugins: {
-    'postcss-nesting': {}, // <-- Corrigido aqui
+    'postcss-nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
