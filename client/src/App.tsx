dd6abc43377f8b1068b21f7721a972b5da3796f7
Levitag12 <levitaa.g12@gmail.com>
2025-07-09 13:06:33 +0000

    correção app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,24 @@
-// client/src/App.tsx
-
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+// Importações das suas páginas
 import LoginPage from './pages/login.tsx';
+import AdminDashboardPage from './pages/admin-dashboard.tsx';
+import ConsultantDashboardPage from './pages/consultant-dashboard.tsx';
+import NotFoundPage from './pages/not-found.tsx';
 
 function App() {
   return (
     <Routes>
+      {/* Rota Principal */}
       <Route path="/" element={<LoginPage />} />
+
+      {/* Rotas dos Dashboards */}
+      <Route path="/admin-dashboard" element={<AdminDashboardPage />} />
+      <Route path="/consultant-dashboard" element={<ConsultantDashboardPage />} />
+
+      {/* Rota para qualquer outro caminho não encontrado */}
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
