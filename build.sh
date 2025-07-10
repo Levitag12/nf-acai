b0ce70a56ec183b266aa0907ceba2f3b08c7b4c6
Levitag12 <levitaa.g12@gmail.com>
2025-07-10 19:35:08 +0000

    correção build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
-
-# 1. Instala todas as dependências do backend listadas no package.json
-echo "Instalando dependências do backend..."
 npm install
-
-# 2. Compila o código TypeScript para JavaScript (usando o script 'build' do package.json)
-echo "Compilando o servidor..."
 npm run build
